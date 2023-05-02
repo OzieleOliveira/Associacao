30880cd66ed2fa0f929ecba0568074dbaf871861
OZIELE RODRIGUES DE OLIVEIRA <oziele.rodrigues@academico.ifrn.edu.br>
2023-05-02 10:15:45 -0300

    alteracao

--- CRUD básico PHP com MySQL/login.php
+++ CRUD básico PHP com MySQL/login.php
@@ -4,10 +4,14 @@
     <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="estilo.css">
     <link rel="icon" type="image/png" href="icon2.png">
+    <link rel="stylesheet"  media="'screen" href="main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/fontawesome.min.css" integrity="sha512-giQeaPns4lQTBMRpOOHsYnGw1tGVzbAIHUyHRgn7+6FmiEgGGjaG0T2LZJmAPMzRCl+Cug0ItQ2xDZpTmEc+CQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/brands.min.css" integrity="sha512-G/T7HQJXSeNV7mKMXeJKlYNJ0jrs8RsWzYG7rVACye+qrcUhEAYKYzaa+VFy6eFzM2+/JT1Q+eqBbZFSHmJQew==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/solid.min.css" integrity="sha512-6mc0R607di/biCutMUtU9K7NtNewiGQzrvWX4bWTeqmljZdJrwYvKJtnhgR+Ryvj+NRJ8+NnnCM/biGqMe/iRA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   </head>
   <body>
     <a href="index.php"><img id="icon"src ="icon2.png" title="Pagina inicial"></a>
-    <h1>Login</h1>  
+    <h1>Login</h1> 
 
     <form action="autenticar.php" method="POST">
         <?php
@@ -26,7 +30,13 @@
         <input type="submit" value="Autenticar" name="autenticar" >       
        
     </form>
-
-    <footer>Todos os direitos reservados &copy Associação dos Pequenos Produtores Rurais</footer>
+    <footer>
+  <p>
+    <h3>Contatos:</h3>
+  </p>
+  <h4 ><i class="fa-solid fa-envelope"></i> Email: <a href="mailto:gr49168@gmail.com">gr49168@gmail.com</a>  </h4>
+  <h4><i class="fa-solid fa-phone"></i>Telefone: <a href="tel:+559996240074">(84)99624-0074</a></h4>
+</footer>
+<footer>Todos os direitos reservados &copy Associação dos Pequenos Produtores Rurais</footer>
 </body>
 </html>
